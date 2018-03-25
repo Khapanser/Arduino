5d20e61b3fee7d394c5acf03895defacb7e22df3
khapanser <khapanser@gmail.com>
2018-03-25 22:55:02 +0300

    StopAfterClientRequest

--- src/main/java/ArduinoClient.java
+++ src/main/java/ArduinoClient.java
@@ -3,16 +3,21 @@ package main.java;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.Buffer;
 
 public class ArduinoClient {
-
+    BufferedReader reader;
     private JFrame frame;
     //private JTextArea area;
     private JButton onButton;
     private JButton offButton;
     private JButton exitButton;
+    private JTextArea outWindow;
+    //private JTextArea inWindow;
     Socket socket;
     PrintWriter writer;
     public static void  main(String[] args){
@@ -27,23 +32,40 @@ public class ArduinoClient {
         onButton = new JButton("ON");
         offButton = new JButton("OFF");
         exitButton = new JButton("EXIT");
+        outWindow = new JTextArea();
+        //inWindow = new JTextArea();
+        JScrollPane scroller = new JScrollPane(outWindow);
+        outWindow.setLineWrap(true);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
-        frame.getContentPane().add(BorderLayout.CENTER,onButton);
+        frame.getContentPane().add(BorderLayout.CENTER,scroller);
         frame.getContentPane().add(BorderLayout.WEST,offButton);
         frame.getContentPane().add(BorderLayout.EAST,exitButton);
+        frame.getContentPane().add(BorderLayout.SOUTH,onButton);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,100);
+        frame.setSize(500,500);
         frame.setVisible(true);
+
+        //Второй поток для считывания
+        Thread thread = new Thread(new IncomingReader());
+        thread.start();
 
         onButton.addActionListener(event -> {
             writer.println("on");
             writer.flush();
+            try {
+                Thread.sleep(20);
+            } catch (Exception er){er.printStackTrace();}
         });
 
         offButton.addActionListener(event -> {
             writer.println("off");
             writer.flush();
+            try {
+                Thread.sleep(20);
+            } catch (Exception er){er.printStackTrace();}
         });
 
         exitButton.addActionListener(event -> {
@@ -57,11 +79,33 @@ public class ArduinoClient {
 
     public void setUpNetworking(){
         try{
-            socket = new Socket("127.0.0.1",7010);
+            socket = new Socket("77.37.181.102",7010);
+            //socket = new Socket("127.0.0.1",7010);
             System.out.println(socket.isConnected());
             writer = new PrintWriter(socket.getOutputStream());
+            InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
+            reader = new BufferedReader(streamReader);
+            System.out.println("networking established");
         }catch(Exception e){e.printStackTrace();}
 
+    }
+
+    public class IncomingReader implements Runnable{
+        /*BufferedReader reader;
+        public IncomingReader(BufferedReader reader){
+            this.reader = reader;
+        }*/
+
+        public void run(){
+            String message;
+            try{
+                while((message = reader.readLine())!= null){
+                    System.out.print("Сообщение с Ардуино: " + message);
+                    outWindow.append("Сообщение с Ардуино: " + message + "\n");
+                    Thread.sleep(100);
+                }
+            }catch (Exception r){r.printStackTrace();}
+        }
     }
 
 
--- src/main/java/ArduinoServer.java
+++ src/main/java/ArduinoServer.java
@@ -3,6 +3,7 @@ package main.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,6 +15,7 @@ import arduino.Arduino;
 public class ArduinoServer {
     int serverPort = 7010;
     String message;
+    static int count = 0;
 
     public static void main(String[] args) throws InterruptedException {
         ArduinoServer server = new ArduinoServer();
@@ -23,47 +25,122 @@ public class ArduinoServer {
     public void go() {
         //String message;
         try {
+            // 1. Организовываем соединение с клиентом:
             ServerSocket socket = new ServerSocket(serverPort);
             Socket clientSocket = socket.accept();
             System.out.println("Выполнено подключение клиента...");
             System.out.println("IP: "+ socket.getInetAddress());
             System.out.println("");
-            //PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
             InputStreamReader isReader = new InputStreamReader(clientSocket.getInputStream());
             BufferedReader reader = new BufferedReader(isReader);
 
             // Указываем на каком порту подключён Arduino
+            Arduino arduino = new Arduino("COM5", 9600);
+            //Открываем подключение к arduino по указанному порту
+            boolean connected = arduino.openConnection();
+            System.out.println("Соединение установлено: " + connected);
+            String ardMessage;
 
-            Arduino arduino = new Arduino("COM4", 9600);
+            //Добавляем второй поток для считывания с ардуино
+            Thread thread = new Thread(new ClientHandler(reader,arduino));
+            thread.start();
 
+
+            //while((ardMessage = arduino.serialRead())!=null){
+            //добавлено две строчки вместе
+            while (true){
+                ardMessage = arduino.serialRead();
+
+                System.out.println("Пришло сообщение с Arduino: "+ardMessage);
+                if (ardMessage!=null){
+                    writer.println("Пришло NOT NULL сообщение с ардуино:");
+                    writer.println(message);
+                    writer.flush();
+                    //Добавлен sleep для смены потока, чтобы не засиживался
+                    //Thread.sleep(100);
+                }
+                else {
+                    writer.println("Пришло NULL сообщение с ардуино!");
+                    writer.flush();
+                }
+            }
+
+
+   /*
             while ((message = reader.readLine()) != null) {
 
-                /**
-                 * Код для отправки на Arduino
-                 */
+
+                 //Код для отправки на Arduino
+
                 System.out.println("С клиента получено: \"" + message + "\"");
                 boolean connected = arduino.openConnection();
                 System.out.println("Соединение установлено: " + connected);
-
-                Thread.sleep(2000);
+                //Уменьшил с 2000 до 10
+                Thread.sleep(10);
 
                 switch (message) {
                     case "on":
-                        arduino.serialWrite('1');
+                        //arduino.serialWrite('k');
+                        arduino.serialWrite("NIKITA ");
                         break;
                     case "off":
-                        arduino.serialWrite('0');
+                        //arduino.serialWrite('1');
+                        arduino.serialWrite("KAPILA!");
                         break;
                     case "exit":
                         arduino.serialWrite('0');
                         arduino.closeConnection();
                 }
 
-                //}catch (Exception ee){ee.printStackTrace();}
             }
-
+*/
         } catch (Exception er) {
             er.printStackTrace();
+        }
+    }
+
+    public class ClientHandler implements Runnable{
+        Socket sock;
+        BufferedReader reader;
+        Arduino arduino;
+        public ClientHandler (Socket clientSocket){
+            sock = clientSocket;
+        }
+        public ClientHandler (BufferedReader reader,Arduino arduino){
+            this.arduino = arduino;
+            this.reader = reader;
+        }
+
+        public void run(){
+
+            try {
+                while ((message = reader.readLine()) != null) {
+                    /*switch (message) {
+                        case "on":
+                            //arduino.serialWrite('0');
+                            arduino.serialWrite("NIKITA ");
+                            break;
+                        case "off":
+                            //arduino.serialWrite('1');
+                            arduino.serialWrite("KAPILA!");
+                            break;
+                        case "exit":
+                            arduino.serialWrite('0');
+                            arduino.closeConnection();
+                    }*/
+                    //Просто передаём строку, которую прислал сервер.
+                    System.out.println("Сейчас работает поток: " + Thread.currentThread());
+                    count++;
+                    System.out.println("Он работает " +  count+" раз");
+                    arduino.serialWrite(message);
+
+                    Thread.sleep(100);
+                    //break;
+                    //
+
+                }
+            } catch (Exception e){e.printStackTrace();}
         }
     }
 }
